c814a6a27647f68f034090ba9d562fbb994481c6
Julius Kibbassa <148614048+Julius-Kibbs@users.noreply.github.com>
2025-09-16 03:29:01 +0300

    Update answers.sql
    adding answers for week 5 assignment

---
+++ answers.sql
@@ -0,0 +1,12 @@
+--Question one 
+ALTER table customers DROP index IdxPhone;
+
+--Question TWO
+CREATE user 'bob'@'localhost' IDENTIFIED BY 'S$cu3r3!';
+
+--Question three
+GRANT INSERT ON salesdb TO 'bob'@'localhost';
+
+--Question four
+ALTER USER 'bob'@'localhost' IDENTIFIED BY 'P$55!23'
+
